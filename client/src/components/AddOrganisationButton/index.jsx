071b8b11b7b28a45d1526b688aacab22af7c0504
metildachee <metildachee@gmail.com>
2020-09-01 16:10:43 +0800

    Client: Add AddOrganisationButton

---
+++ client/src/components/AddOrganisationButton/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Button } from "antd";
+import CollectionCreateForm from "./OrganisationCreateForm";
+import { getCategories, addCategory } from "../../logic/category";
+import openNotificationWithIcon from "../Notification";
+import { store } from "../GlobalStoreProvider";
+
+export default function CollectionsPage() {
+  const [visible, setVisible] = useState(false);
+  const { dispatch } = useContext(store);
+
+  useEffect(() => {
+    getCategories(dispatch);
+  }, []);
+
+  const onCreate = (values) => {
+    setVisible(false);
+    addCategory(dispatch, values);
+    openNotificationWithIcon(
+      "success",
+      `${values.name} added successfully!`,
+      `A new organisation has been added.`
+    );
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        Add Organisation
+      </Button>
+      <CollectionCreateForm
+        visible={visible}
+        onCreate={onCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+}
--- client/src/pages/Dashboard/index.jsx
+++ client/src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import CustomerTable from "./Client_Table";
 import AddButton from "./Client_AddTicketButton";
 import AddCategoryButton from "../../components/AddCategoryButton";
+import AddOrganisationButton from "../../components/AddOrganisationButton";
 import { store } from "../../components/GlobalStoreProvider";
 import { isClient, isAdmin } from "../../logic/authentication";
 import AdminDashboard from "./Admin_Table";
@@ -15,8 +16,9 @@ function Dashboard() {
   return (
     <>
       <Row justify="space-around" style={{ padding: "10px" }}>
-        <Col>
-          {client && <AddButton />} {admin && <AddCategoryButton />}
+        <Col style={{ display: "flex" }}>
+          {client && <AddButton />} {admin && <AddCategoryButton />}{" "}
+          {admin && <AddOrganisationButton />}
         </Col>
       </Row>
       <Row justify="space-around">
