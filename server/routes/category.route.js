90b4b03f369dcf65f5463632df1cd5ef51ee9de4
metildachee <metildachee@gmail.com>
2020-09-01 15:32:22 +0800

    Server: Update POST route with user authentication for Category

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Category = require("../model/category.model");
+const checkToken = require("../lib/config");
 
 /* 
     @route GET api/categories
@@ -46,13 +47,15 @@ router.put("/", async (req, res) => {
     TODO: Should only be accessible to admins
 */
 
-router.post("/", async (req, res) => {
+router.post("/", checkToken, async (req, res) => {
+  console.log(req.body);
   try {
     let category = await Category.create(req.body);
     res
       .status(200)
       .send({ msg: `${req.body.name} successfully added!`, category });
   } catch (error) {
+    console.error(error);
     res
       .status(500)
       .send({ msg: `${req.body.name} cannot be added into categories.` });
