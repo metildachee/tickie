e9a3af5e7e4dcd2642b2ec93871c941219d7e0a0
metildachee <metildachee@gmail.com>
2020-09-02 22:31:05 +0800

    First push

--- server/lib/database.js
+++ server/lib/database.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.CONTAINER_DB,
+  process.env.PRODUCTION_DB,
   {
     useCreateIndex: true,
     useNewUrlParser: true,
