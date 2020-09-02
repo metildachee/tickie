6585a8d7166286fe5f000d28e5d4367e5071fbf6
metildachee <metildachee@gmail.com>
2020-09-02 16:43:08 +0800

    Updates

---
+++ precache-manifest.aa56988692e99c05c9aa0bbd158b9d9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0190b91207284f3d948e0f1f8cb4d768",
+    "url": "/tickie/index.html"
+  },
+  {
+    "revision": "0ac46914fd9e9bb4a4e1",
+    "url": "/tickie/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "f570c6fbda24b13c99d0",
+    "url": "/tickie/static/css/main.27e02447.chunk.css"
+  },
+  {
+    "revision": "0ac46914fd9e9bb4a4e1",
+    "url": "/tickie/static/js/2.adc180d8.chunk.js"
+  },
+  {
+    "revision": "ad2a958eb0eea9a27c11a19353ff5620",
+    "url": "/tickie/static/js/2.adc180d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f570c6fbda24b13c99d0",
+    "url": "/tickie/static/js/main.b47da9c6.chunk.js"
+  },
+  {
+    "revision": "6c2f7c9d96b8321e7c4d",
+    "url": "/tickie/static/js/runtime-main.6d6d8fe5.js"
+  },
+  {
+    "revision": "91f3cb172aacc50122d0dd65946ec9ba",
+    "url": "/tickie/static/media/kpi.91f3cb17.jpg"
+  },
+  {
+    "revision": "a9617a44f1974b1024e5f047a48767b4",
+    "url": "/tickie/static/media/productivity.a9617a44.jpg"
+  }
+]);
